33acd5de93de4f30d91f74d98740066aac8fad50
minafrajr <carlos.minafrajr@gmail.com>
2020-10-14 00:06:05 -0300

    migration images

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,9 @@ import OrphangeController from './controllers/OrphanagesController';
 
 const routes = Router();
 
+//index, show, update, delete
 routes.post('/orphanages', OrphangeController.create);
+routes.get('/orphanages', OrphangeController.index);
+routes.get('/orphanages/:id', OrphangeController.show);
 
 export default routes;
