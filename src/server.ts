8deba860fba09c75f399d09fd16cc4870837b4c7
minafrajr <carlos.minafrajr@gmail.com>
2020-10-15 09:50:07 -0300

    exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,39 @@
-import express, { query } from 'express';
+//  Rota = conjunto
+//  Recurso = usuário
+
+//  Métodos HTTP = GET, POST, PUT, DELETE, PATCH
+
+//  Parâmetros
+
+//  GET = Buscar uma informação (lista/item)
+//  POST = Criando uma informação
+//  PUT = Editando uma informação
+//  DELETE = Deletando uma informação
+
+//  Query: http:loacalhost:3333/users?search=diego
+//  Route Params: http:localhost:3333/users/1 (identificar um recurso)
+//  Body: http:localhost:3333/users/
+
+//  app.post('/users/:id',(req,res)=>{
+
+//        console.log(req.query);
+//        console.log(req.params);
+//        console.log(req.body);
+
+//        return res.json({message: 'Hello World'})
+//  })
+
+import express from 'express';
+import 'express-async-errors';
 import './database/connection';
 import routes from './routes';
 import path from 'path';
+import errorHandler from './errors/handler';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-/** 
- * {
-// Rota = conjunto
-// Recurso = usuário
-
-// Métodos HTTP = GET, POST, PUT, DELETE, PATCH
-
-// Parâmetros
-
-// GET = Buscar uma informação (lista/item)
-// POST = Criando uma informação
-// PUT = Editando uma informação
-// DELETE = Deletando uma informação
-
-// Query: http://loacalhost:3333/users?search=diego
-// Route Params: http://localhost:3333/users/1 (identificar um recurso)
-// Body: http://localhost:3333/users/
-
-// app.post('/users/:id',(req,res)=>{
-
-//       console.log(req.query);
-//       console.log(req.params);
-//       console.log(req.body);
-
-//       return res.json({message: 'Hello World'})
-// })
-}*/
-
-app.get('/users', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+app.use(errorHandler);
 
 app.listen(3333);
-
-// banco de dados
-// driver FocusNavigationEvent, query MSBlobBuilder, orm
